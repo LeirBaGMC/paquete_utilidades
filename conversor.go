235f1b9906e025aaa7505b590db515d97731915f
LeirBaGMC <gabominda@outlook.com>
2025-09-25 19:16:54 -0500

    Agregando código de las funciones

---
+++ conversor.go
@@ -0,0 +1,27 @@
+package utilidades
+
+/*El usuario ingresará un valor en dólares y la moneda a la que quiere convertir y
+el programa mostrará ese valor en dicha moneda. Las monedas permitidas son:
+ Euros
+LB (Libras Esterlinas)
+Won (Sur Koreano)
+BTC
+*/
+// Conversor... convierte un valor en dólares a la moneda indicada
+// Opción: 1=EUR, 2=LB, 3=WON, 4=BTC
+func conversor(valor float64, opcion int) float64 {
+
+	switch opcion {
+	case 1:
+		return valor * 0.86
+	case 2:
+		return valor * 0.75
+	case 3:
+		return valor * 1409.91
+	case 4:
+		return valor * 0.0000092
+	default:
+		return 0
+	}
+
+}
---
+++ vocales.go
@@ -0,0 +1,15 @@
+package utilidades
+
+// contadorVocales recibe una palabra y devuelve el número total de vocales
+func contadorVocales(palabra string) int {
+	contador := 0
+
+	for _, letra := range palabra {
+		if letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u' ||
+			letsra == 'A' || letra == 'E' || letra == 'I' || letra == 'O' || letra == 'U' {
+			contador += 1
+		}
+	}
+
+	return contador
+}
